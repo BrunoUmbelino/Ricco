2e4f83ccf3c14dd1260aa08f5256535995abbbf0
Bruno <bruno.umbelino0@gmail.com>
2021-03-25 16:03:08 -0300

    adding Navbar and Sidebar components

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarWrapper,
+  SideBtnWrap,
+  SidebarRoute,
+  SidebarMenu,
+} from "./SidebarElements";
+
+function Sidebar() {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+}
+
+export default Sidebar;
